2c13ab815e7d2c47c6bbdcca5025c235d5a0c0f7
Calvinan132 <trinhhoaian2303@gmail.com>
2025-11-13 08:19:39 +0700

    sửa chính tả1

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Chatting from "./pages/Chatting";
